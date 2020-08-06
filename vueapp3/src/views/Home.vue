d1632b7a5d42508f23d352b8b92410e23f00e1af
Tirawat Sampaothong <boss.emute1g@gmail.com>
2020-08-06 16:13:06 +0700

    #4 ทดสอบ vuetify

--- vueapp3/src/views/Home.vue
+++ vueapp3/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-btn small color="primary">HOME</v-btn>
+    <v-btn small color="error">Error</v-btn>
+      <v-btn icon color="pink">
+            <v-icon>mdi-heart</v-icon>
+            </v-btn>
+          
+ 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+
+</style>
