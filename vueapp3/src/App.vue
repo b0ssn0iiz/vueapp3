03d0fc69e3a733fbef0883d36fcc750dd367e078
Tirawat Sampaothong <boss.emute1g@gmail.com>
2020-08-20 16:13:15 +0700

    #6 เพิ่มหน้า เพิ่มปุ่ม ใส่กราฟ

--- vueapp3/src/App.vue
+++ vueapp3/src/App.vue
@@ -1,41 +1,58 @@
 <template>
-<div>
-<v-app>
-  <v-navigation-drawer app>
-    <p>navigation drawer</p>
-     <v-btn large color="primary">ฟิตข่าว</v-btn> <br>
-     
-     <v-btn large color="error">Messenger</v-btn>
-  </v-navigation-drawer>
+  <div>
+    <v-app>
+      <v-navigation-drawer app clipped v-model="drawer">
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-btn to="/" class="mb-3" color="primary" block>หน้าแรก</v-btn>
 
-  <v-app-bar app>
-    <p>app bar</p>
-  </v-app-bar>
-  
-  <v-main>
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
-  </v-main>
+              <v-btn to="/customers" class="mb-3" color="success" block>ลูกค้า</v-btn>
 
-  <v-footer app>
-    <p>footer</p>
-  </v-footer>
-  
-</v-app>
-  </div>  
+              <v-btn to="/products" class="mb-3" color="warning" block>สินค้า</v-btn>
+
+              <v-btn to="/reports" class="mb-3" color="error" block>รายงาน</v-btn>
+
+              <v-btn to="/about" class="mb-3" color="indigo" block>เกี่ยวกับ</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-navigation-drawer>
+
+      <v-app-bar app clipped-left>
+        <v-btn class="ma-3" outlined fab @click="drawer = !drawer">
+        <v-icon color="dark">mdi-format-list-bulleted-square</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+          ระบบเก็บข้อมูลร้านค้า
+        </v-toolbar-title>
+      </v-app-bar>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+
+      <v-footer app>
+        <span>&copy; RMUTL {{ new Date().getFullYear() + 543 }}</span>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "RMUTL"
-    }
-  }
-}
+      message: "RMUTL",
+      drawer: null
+    };
+  },
+};
 </script>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap');
+#app {
+  font-family: 'Mitr', sans-serif;
+}
 </style>
--- vueapp3/src/views/Home.vue
+++ vueapp3/src/views/Home.vue
@@ -1,21 +1,86 @@
 <template>
-  <div>
-    <v-btn small color="primary">HOME</v-btn>
-    <v-btn small color="error">Error</v-btn>
-      <v-btn icon color="pink">
-            <v-icon>mdi-heart</v-icon>
-            </v-btn>
-          
- 
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-card color="#FF5C00" dark>
+          <v-card-title><div class="font-big">จำนวนลูกค้า</div></v-card-title>
+
+          <v-card-text><div class="font-big">50 คน</div></v-card-text>
+
+          <v-card-actions>
+            <v-btn text>ข้อมูลลูกค้า</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col cols="4">
+        <v-card color="#FF7C00" dark>
+          <v-card-title><div class="font-big">จำนวนสินค้า</div></v-card-title>
+
+          <v-card-text><div class="font-big">20 ชิ้น</div></v-card-text>
+
+          <v-card-actions>
+            <v-btn text>ข้อมูลสินค้า</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col cols="4">
+        <v-card color="#ff8c00" dark>
+          <v-card-title><div class="font-big">จำนวนรายงาน</div></v-card-title>
+
+          <v-card-text><div class="font-big">16 รายการ</div></v-card-text>
+
+          <v-card-actions>
+            <v-btn text>ข้อมูลรายงาน</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto text-center" color="gold" dark>
+          <v-card-text>
+            <v-sheet color="rgba(0, 0, 0, .12)">
+              <v-sparkline
+                :value="value"
+                color="rgba(255, 140, 0, 1)"
+                height="70"
+                padding="15"
+                stroke-linecap="round"
+              >
+                <template v-slot:label="item"> {{ item.value }}฿ </template>
+              </v-sparkline>
+            </v-sheet>
+          </v-card-text>
+          <v-card-text>
+            <div class="font-big font-weight-thin">รายงานการขาย</div>
+          </v-card-text>
+
+          <v-divider></v-divider>
+          <!-- 
+          <v-card-actions class="justify-center">
+            <v-btn block text>รายงานการขาย</v-btn>
+          </v-card-actions> -->
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      value: [111, 999, 111, 999, 111, 999, 111,]
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scope>
+.font-big {
+  font-size: 30px;
+}
 </style>
